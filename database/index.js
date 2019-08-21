46a6a0348abba7f8ac69b18f18307e6fbf2a80a6
Wendy Wu <Wendy@Wendys-MacBook-Air.local>
2019-08-21 12:20:03 -0700

    Set up webpack, add component files, develop schema

---
+++ database/index.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/photogallery', {useNewUrlParser: true});
+
+var photoSchema = new mongoose.Schema({
+    id: Number,
+    listing_id: Number,
+    photo_url: String,
+    desription: String
+});
+
+var PhotoGallery = mongoose.model('PhotoGallery', photoSchema);
+
+let save = (photos, callback) => {
+
+    photos.forEach((photo) => {
+        var newPhotos = new PhotoGallery({
+            id: photo.id,
+            listing_id: photo.listing_id,
+            photo_url: photo.photo_url,
+            description: photo.description
+        })
+        PhotoGallery.create({
+            id: photo.id,
+            listing_id: photo.listing_id,
+            photo_url: photo.photo_url,
+            description: photo.description
+        }, (err, newPhotos) => {
+            if (err) return handleError(err); 
+        })
+    })
+}
+
+console.log('Connected to Mongoose'); 
+
+module.exports = {
+    save: save,
+}
+
